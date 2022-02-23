7dab13c6e8856428406683e7f89f67de7b45cd73
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-02-23 15:45:24 -0500

    Package / file refactoring

--- src/test/java/com/expressionparser/ExpressionParserPluginTest.java
+++ src/test/java/com/expressionparser/ExpressionParserPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.expressionparser;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ExpressionParserPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ExpressionParserPlugin.class);
 		RuneLite.main(args);
 	}
 }
