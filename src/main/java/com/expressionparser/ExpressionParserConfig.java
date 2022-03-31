dcb880304bc820179f0737da35ecec92876240f4
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-03-31 17:40:19 -0400

    Completed Implementation + Autogen files

--- src/main/java/com/expressionparser/ExpressionParserConfig.java
+++ src/main/java/com/expressionparser/ExpressionParserConfig.java
@@ -7,13 +7,5 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("Expression Parser")
 public interface ExpressionParserConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "On-Start message",
-		description = "Display plugin description on client login"
-	)
-	default String greeting()
-	{
-		return "An expression parsing plugin.";
-	}
+
 }
--- src/main/java/com/expressionparser/ExpressionParserPlugin.java
+++ src/main/java/com/expressionparser/ExpressionParserPlugin.java
@@ -6,18 +6,28 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.MessageNode;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.chat.ChatColorType;
+import net.runelite.client.chat.ChatMessageBuilder;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import com.expressionparser.javacc.*;
 @Slf4j
 @PluginDescriptor(
-        name = "ExpressionParser"
+        name = "Expressions"
 )
 public class ExpressionParserPlugin extends Plugin
 {
+    Expressions eval = null;
+
     @Inject
     private Client client;
 
@@ -37,12 +47,40 @@ public class ExpressionParserPlugin extends Plugin
     }
 
     @Subscribe
+    public void onChatMessage(ChatMessage chatMessage) throws ParseException {
+        float result = 0;
+/*
+        if(chatMessage.getType() != ChatMessageType.PUBLICCHAT) {
+            return;
+        }
+*/
+        String message = chatMessage.getMessage();
+        try {
+            if (eval == null) {
+                eval = new Expressions(new ByteArrayInputStream(message.getBytes()));
+            } else {
+                eval.ReInit(new ByteArrayInputStream(message.getBytes()));
+            }
+            result = eval.Start();
+            log.info(String.valueOf(result));
+            String redrawMessage = new ChatMessageBuilder()
+                    .append(ChatColorType.NORMAL)
+                    .append(message)
+                    .append(ChatColorType.HIGHLIGHT)
+                    .append(" = " + result)
+                    .build();
+
+            final MessageNode messageNode = chatMessage.getMessageNode();
+            messageNode.setRuneLiteFormatMessage(redrawMessage);
+            client.refreshChat();
+
+        } catch (Error e) { }
+    }
+
+    @Subscribe
     public void onGameStateChanged(GameStateChanged gameStateChanged)
     {
-        if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-        {
-            client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-        }
+        /* log.info("Game-State Changed!"); */
     }
 
     @Provides
