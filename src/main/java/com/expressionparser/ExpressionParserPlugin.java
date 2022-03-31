8ce7eddf0779a79145763f35c5ed0eaa276df19d
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-03-31 18:26:45 -0400

    Commented + Deleted unnecessary code

--- src/main/java/com/expressionparser/ExpressionParserPlugin.java
+++ src/main/java/com/expressionparser/ExpressionParserPlugin.java
@@ -1,24 +1,16 @@
 package com.expressionparser;
 
-import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
 import net.runelite.api.MessageNode;
 import net.runelite.api.events.ChatMessage;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatMessageBuilder;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import com.expressionparser.javacc.*;
 @Slf4j
 @PluginDescriptor(
@@ -31,29 +23,10 @@ public class ExpressionParserPlugin extends Plugin
     @Inject
     private Client client;
 
-    @Inject
-    private ExpressionParserConfig config;
-
-    @Override
-    protected void startUp() throws Exception
-    {
-        /* log.info("Example started!"); */
-    }
-
-    @Override
-    protected void shutDown() throws Exception
-    {
-        /* log.info("Example stopped!"); */
-    }
-
     @Subscribe
     public void onChatMessage(ChatMessage chatMessage) throws ParseException {
         float result = 0;
-/*
-        if(chatMessage.getType() != ChatMessageType.PUBLICCHAT) {
-            return;
-        }
-*/
+
         String message = chatMessage.getMessage();
         try {
             if (eval == null) {
@@ -77,15 +50,4 @@ public class ExpressionParserPlugin extends Plugin
         } catch (Error e) { }
     }
 
-    @Subscribe
-    public void onGameStateChanged(GameStateChanged gameStateChanged)
-    {
-        /* log.info("Game-State Changed!"); */
-    }
-
-    @Provides
-    ExpressionParserConfig provideConfig(ConfigManager configManager)
-    {
-        return configManager.getConfig(ExpressionParserConfig.class);
-    }
 }
