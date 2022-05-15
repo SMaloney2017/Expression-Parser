3531fe58f1809c916eeb1c2654f051dce0d6c8df
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-05-14 20:31:31 -0400

    IntelliJ code-reformatting via XML

--- src/main/java/com/expressionparser/ExpressionParserPlugin.java
+++ src/main/java/com/expressionparser/ExpressionParserPlugin.java
@@ -12,42 +12,50 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import java.io.ByteArrayInputStream;
 import com.expressionparser.javacc.*;
+
 @Slf4j
 @PluginDescriptor(
-        name = "Expressions"
+	name = "Expressions"
 )
 public class ExpressionParserPlugin extends Plugin
 {
-    Expressions eval = null;
+	Expressions eval = null;
 
-    @Inject
-    private Client client;
+	@Inject
+	private Client client;
 
-    @Subscribe
-    public void onChatMessage(ChatMessage chatMessage) throws ParseException {
-        float result = 0;
+	@Subscribe
+	public void onChatMessage(ChatMessage chatMessage) throws ParseException
+	{
+		float result = 0;
 
-        String message = chatMessage.getMessage();
-        try {
-            if (eval == null) {
-                eval = new Expressions(new ByteArrayInputStream(message.getBytes()));
-            } else {
-                eval.ReInit(new ByteArrayInputStream(message.getBytes()));
-            }
-            result = eval.Start();
-            /* log.info(String.valueOf(result)); */
-            String redrawMessage = new ChatMessageBuilder()
-                    .append(ChatColorType.NORMAL)
-                    .append(message)
-                    .append(ChatColorType.HIGHLIGHT)
-                    .append(" = " + result)
-                    .build();
+		String message = chatMessage.getMessage();
+		try
+		{
+			if (eval == null)
+			{
+				eval = new Expressions(new ByteArrayInputStream(message.getBytes()));
+			}
+			else
+			{
+				eval.ReInit(new ByteArrayInputStream(message.getBytes()));
+			}
+			result = eval.Start();
+			String redrawMessage = new ChatMessageBuilder()
+				.append(ChatColorType.NORMAL)
+				.append(message)
+				.append(ChatColorType.HIGHLIGHT)
+				.append(" = " + result)
+				.build();
 
-            final MessageNode messageNode = chatMessage.getMessageNode();
-            messageNode.setRuneLiteFormatMessage(redrawMessage);
-            client.refreshChat();
+			final MessageNode messageNode = chatMessage.getMessageNode();
+			messageNode.setRuneLiteFormatMessage(redrawMessage);
+			client.refreshChat();
 
-        } catch (Error e) { }
-    }
+		}
+		catch (Error e)
+		{
+		}
+	}
 
 }
